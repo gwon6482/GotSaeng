5ad988dc26c67267f755eff86da945567cd5032e
gwon6482 <pdsjw991020@gmail.com>
2023-11-06 17:55:41 +0900

    loginpage

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(path.join(__dirname,'../views/login.html'))
+  res.sendFile(path.join(__dirname,'../views/login.html'))
 });
 
 module.exports = router;
