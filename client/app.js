1f410d7ef989d51b6512b3e9b61d3b22dfb43e79
winkslee <jakejhlee0210@gmail.com>
2023-11-07 15:29:42 +0900

    as

--- client/app.js
+++ client/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 
 
+
 var app = express();
 
 // view engine setup
@@ -18,9 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/', indexRouter);
+
 
 
 // catch 404 and forward to error handler
--- client/routes/index.js
+++ client/routes/index.js
@@ -1,11 +1,12 @@
+// 모든 라우터 파일은 이거랑 형식 똑같음
+
 var express = require('express');
 var router = express.Router();
 var path = require('path')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //console.log(path.join(__dirname,'../views/index.html'))
-  res.sendFile(path.join(__dirname,'../views/index.html'))
+  res.sendFile(path.join(__dirname,'../dist/index.html'))
 });
 
 module.exports = router;
